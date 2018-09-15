da6791ed34156bebcad33a5afb8534b894215e69
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-09-15 18:04:22 -0300

    Add files via upload

---
+++ Lista4_07.py
@@ -0,0 +1,4 @@
+numeros = [1, 2, 3, 4, 5]
+multiplicar = numeros[0] * numeros[1] * numeros[2] * numeros[3] * numeros[4]
+
+print("\nSoma: ", sum(numeros), "\nMultiplicão: ", multiplicar, "\nNúmeros: ", numeros)
