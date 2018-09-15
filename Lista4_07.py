4a03078d7aee2c39429a2badbc2f37ad02158190
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-09-15 20:38:05 -0300

    Update Lista4_07.py

--- Lista4_07.py
+++ Lista4_07.py
@@ -1,4 +1,4 @@
+from functools import reduce
 numeros = [1, 2, 3, 4, 5]
-multiplicar = numeros[0] * numeros[1] * numeros[2] * numeros[3] * numeros[4]
-
-print("\nSoma: ", sum(numeros), "\nMultiplicão: ", multiplicar, "\nNúmeros: ", numeros)
+multiplicados = reduce((lambda x, y: x * y), numeros)
+print("\nSoma: ", sum(numeros), "\nMultiplicão: ", multiplicados, "\nNúmeros: ", numeros)
